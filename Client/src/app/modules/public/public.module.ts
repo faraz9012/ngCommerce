b173ee358b265fa53efd5c713e54f458079618c3
faraz9012 <54522532+faraz9012@users.noreply.github.com>
2023-12-26 12:58:00 +0500

    [feat]: admin and frontend module created

---
+++ Client/src/app/modules/public/public.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomepageComponent } from './components/homepage/homepage.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomepageComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    HomepageComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class PublicModule { }
