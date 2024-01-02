79b4050b902df13913813968c3467aede817e6b4
faraz9012 <54522532+faraz9012@users.noreply.github.com>
2024-01-02 17:58:51 +0500

    login form completed and bootstrap grid added

--- Client/src/app/modules/public/components/login/login.component.ts
+++ Client/src/app/modules/public/components/login/login.component.ts
@@ -1,43 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
-import { Observable, of } from "rxjs";
+import { Observable, map, of, shareReplay } from "rxjs";
 
 import { AccountService } from "../../../../services/account.service";
 import { User } from "../../models/user";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent implements OnInit{
-  model:any={}
+export class LoginComponent implements OnInit {
+  model: any = {}
   currentUser$: Observable<User | null> = of(null);
   loginForm: FormGroup = new FormGroup({});
 
-  constructor(public accountService:AccountService, private router:Router, private fb: FormBuilder){}
-  
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private fb: FormBuilder,
+    private breakpointObserver: BreakpointObserver
+  ) { }
+
   ngOnInit(): void {
     this.initializeForm();
   }
 
 
-  initializeForm(){
+  initializeForm() {
     this.loginForm = this.fb.group({
-      email:['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
     });
   }
 
-  onLogin(){
+  onLogin() {
     this.accountService.login(this.loginForm.value).subscribe({
       next: () => {
-          this.router.navigateByUrl('/');
+        this.router.navigateByUrl('/');
       },
-      error: (error)=>{
-          console.log(error.error.message);
+      error: (error) => {
+        console.log(error.error.message);
       }
     });
   }
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.HandsetPortrait || Breakpoints.HandsetLandscape)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 }
