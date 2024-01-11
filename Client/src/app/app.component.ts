326d0bc600c67dbcad265eecad0d174f161c6072
faraz9012 <54522532+faraz9012@users.noreply.github.com>
2024-01-11 16:08:12 +0500

    [fix]: mini cart fixed

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { initFlowbite } from "flowbite";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'ngCommerce';
-
-  ngOnInit(): void {
-    initFlowbite();
-  }
 }
--- Client/src/app/modules/public/public.component.ts
+++ Client/src/app/modules/public/public.component.ts
@@ -1,11 +1,5 @@
-import { Component } from '@angular/core';
-
-export interface Tile {
-    color: string;
-    cols: number;
-    rows: number;
-    text: string;
-  }
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from "flowbite";
 
 @Component({
   selector: 'app-public-root',
@@ -13,12 +7,9 @@ export interface Tile {
   styleUrls: ['public.component.css']
 })
 
-export class PublicComponent {
-    tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  ];
+export class PublicComponent implements OnInit{
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
 
