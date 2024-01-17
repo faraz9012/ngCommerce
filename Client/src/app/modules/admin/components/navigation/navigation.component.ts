bb3393bf8e8c3cb428de8372a83dd5b65a0fa3a0
faraz9012 <54522532+faraz9012@users.noreply.github.com>
2024-01-17 20:15:06 +0500

    [fix]: parent category radio button fix

--- Client/src/app/modules/admin/components/navigation/navigation.component.ts
+++ Client/src/app/modules/admin/components/navigation/navigation.component.ts
@@ -16,28 +16,15 @@ const sideMenuObservable: Observable<sideMenu[]> = of(Side_Navbar).pipe(delay(0)
 
 export class NavigationComponent implements OnInit {
 
-  sideMenu$: Observable<sideMenu[]> = of([]);
-
   //Variables
   logo = '../../../../../favicon.ico';
-  isToggle = false;
-
+  sideMenu$: Observable<sideMenu[]> = of([]);
 
   //Services
   _accountService = inject(AccountService);
   
   ngOnInit(): void {
     this.sideMenu$ = sideMenuObservable;
-  }
-
-  toggleSubMenu(menu: sideMenu) {
-    if (this.sideMenu$) {
-      // Using `subscribe` to modify the data
-      this.sideMenu$.subscribe(data => {
-        menu.isToggle = !menu.isToggle;
-        data.filter(item => item !== menu).forEach(item => item.isToggle = false);
-      });
-    }
   }
   
   logout(){
