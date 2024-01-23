35e444c8ea920da6236bebafe5aea4b35ce6c6ae
faraz9012 <54522532+faraz9012@users.noreply.github.com>
2024-01-23 16:56:16 +0500

    added a wild card route

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard';
+import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,10 @@ export const routes: Routes = [
       path: "admin",
       canActivate: [authGuard],
       loadChildren:() => import("./modules/admin/admin.routes").then(m => m.routes)
+    },
+    {
+      path: '**',
+      loadComponent:() => import("./errors/page-not-found/page-not-found.component").then(m => m.PageNotFoundComponent), 
+      pathMatch: 'full'
     }
   ];
